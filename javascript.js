1f112d895c5c9502ba296a03549e4de486c0de73
fadlalah-taher <fadeltaher111@gmail.com>
2022-05-23 10:30:47 +0300

    Display the two images randomly

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
 
-// First random number 
+// random number > will execute the number of the image(dice)
 const firstRandom = Math.floor(Math.random()*6) + 1;
+const secondRandom = Math.floor(Math.random()*6) + 1;
 
-// images src
+// images src connect with the random number
 const firstImage = 'images/dice' + firstRandom + '.png';
+const secondImage = 'images/dice' + secondRandom + '.png';
 
-document.querySelectorAll('img')[0].setAttribute('src',firstImage);
+// display the images randomly
+document.getElementById("img1").setAttribute('src',firstImage);
+document.getElementById("img2").setAttribute('src',secondImage);
