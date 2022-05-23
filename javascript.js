5f4b22a272eb7b383dd0db218bc75738c6007d81
fadlalah-taher <fadeltaher111@gmail.com>
2022-05-23 10:40:23 +0300

    Display the result on the header and add DOM var

--- javascript.js
+++ javascript.js
@@ -1,12 +1,31 @@
 
-// random number > will execute the number of the image(dice)
+// DOM
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+var header = document.getElementById("header");
+
+
+// random number > will execute the number of the image(dice) randomly
 const firstRandom = Math.floor(Math.random()*6) + 1;
 const secondRandom = Math.floor(Math.random()*6) + 1;
 
-// images src connect with the random number
+// images src connected with the random number
 const firstImage = 'images/dice' + firstRandom + '.png';
 const secondImage = 'images/dice' + secondRandom + '.png';
 
 // display the images randomly
-document.getElementById("img1").setAttribute('src',firstImage);
-document.getElementById("img2").setAttribute('src',secondImage);
+img1.setAttribute('src',firstImage);
+img2.setAttribute('src',secondImage);
+
+
+// Result player1 vs player 2
+
+if(firstRandom > secondRandom){
+    header.innerHTML= "Player 1 Wins!";
+}
+else if(firstRandom < secondRandom){
+    header.innerHTML= "Player 2 Wins!";
+}
+else{
+    header.innerHTML= "Draw!";
+}
